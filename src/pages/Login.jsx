7c00f86e8e450f9ee3499941713b29cfd742c99c
Tarun <tarunnagpal192004@gmail.com>
2024-06-28 12:58:32 +0530

    responsive some elements

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { Login as LoginComponent } from "../components";
 
 export default function Login(){
     return(
-        <div className="sm:p-20  sm:m-10 p-5 mt-52 h-screen sm:h-auto">
+        <div className="sm:p-20  sm:m-10 p-5 mt-44 h-screen sm:h-auto">
               <LoginComponent />
         </div>
     )
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,7 +3,7 @@ import { Signup as Signupcomponent } from "../components";
 export default function Signup(){
     return(
 
-        <div className="  sm:p-20  p-5 mt-52 sm:m-1 h-screen sm:h-auto  ">
+        <div className="  sm:p-20  p-5 mt-32 sm:m-1 h-screen sm:h-auto  ">
             <Signupcomponent />
         </div>
     )
