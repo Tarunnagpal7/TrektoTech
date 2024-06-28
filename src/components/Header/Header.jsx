058566c99224d7ecbd9ded5818864a8382e0d6eb
Tarun <tarunnagpal192004@gmail.com>
2024-06-28 13:09:58 +0530

    Fixing header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -46,7 +46,7 @@ console.log(authStatus)
         }
     ]
     return(
-        <header className="py-3 shadow bg-violet-950 text-white font-bold text-sm ">
+        <header className="sm:py-3 py-2  shadow bg-violet-950 text-white font-bold text-xs  sm:text-sm ">
             <Container>
                 <nav className="flex sm:justify-between justify-around items-center">
                     <div className="mr-4">
@@ -60,7 +60,7 @@ console.log(authStatus)
                             <li key={item.name}>
                                  <button
                                   onClick={()=>navigate(item.slug)}
-                                  className="inline-bock px-6 py-2 duration-200 hover:bg-pink-200 hover:text-violet-950 rounded-full "
+                                  className="inline-bock sm:px-6 px-2 py-2 duration-200 hover:bg-pink-200  hover:text-violet-950 rounded-full "
                                  >{item.name}</button>
                             </li>
                            ) : null
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 export const  Logo=({width = '100px'})=>{
     return (
-        <div className="text-2xl font-mono font-extrabold text-pink-200 line-through">Trek-to-Tech</div>
+        <div className="sm:text-2xl text-md font-mono font-extrabold text-pink-200 line-through">Trek-to-Tech</div>
     )
 }
